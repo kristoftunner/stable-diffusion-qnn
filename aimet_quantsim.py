22a98dd6669f18e86974a3abf2937d0eddfe1468
kitu <kristoftunner95@gmail.com>
2024-02-15 20:57:08 +0000

    modified the script, now models can be separately quantized

--- aimet_quantsim.py
+++ aimet_quantsim.py
@@ -39,7 +39,7 @@ from stable_diff_pipeline import run_text_encoder, run_diffusion_steps, run_vae_
 
 class ExtendedOnnxExportApiArgs(OnnxExportApiArgs):
     def __init__(self, opset_version=None, input_names=None, output_names=None,
-            verbose=False, use_external_data_format=False):
+                 verbose=False, use_external_data_format=False):
         OnnxExportApiArgs.__init__(self, opset_version=opset_version, input_names=input_names,
                                    output_names=output_names)
         self.use_external_data_format = use_external_data_format
@@ -55,7 +55,7 @@ def export_quantsim_model(qsim, output_path, dummy_input, filename_prefix, verbo
                           use_external_data_format=False, input_names=None, output_names=None):
 
     onnx_utils.update_all_onnx_nodes_name = False
-    onnx_utils.simplify_onnx_model = True
+    onnx_utils.simplify_onnx_model = False
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
@@ -77,34 +77,45 @@ def export_quantsim_model(qsim, output_path, dummy_input, filename_prefix, verbo
     qsim.model.to(device)
 
 
-def export_all_models(te_sim, unet_sim, vae_sim, tokens, embeddings, latents):
-
+def export_text_encoder(model, tokens):
     print("Exporting Text Encoder ----")
-    dummy_input = tokens[0][0].to(te_sim.model.device)
-    export_quantsim_model(te_sim, '_exports_/text_encoder_onnx', dummy_input, 'text_encoder',
+    dummy_input = tokens[0][0].to(model.model.device)
+    export_quantsim_model(model, '_exports_/text_encoder_onnx', dummy_input, 'text_encoder',
                           input_names=['input_1'], output_names=['output_1'])
 
+
+def export_unet(model, embeddings):
     print("Exporting UNET ----")
-    dummy_input = unet_dummy_input(unet_sim.model, embeddings)
-    export_quantsim_model(unet_sim, '_exports_/unet_onnx', dummy_input, 'unet',
+    dummy_input = unet_dummy_input(model.model, embeddings)
+    export_quantsim_model(model, '_exports_/unet_onnx', dummy_input, 'unet',
                           use_external_data_format=True,
                           input_names=['input_1', 'input_2', 'input_3'], output_names=['output_1'])
 
+
+def export_vae(model, latents):
     print("Exporting VAE ----")
     dummy_input = latents[0]
-    export_quantsim_model(vae_sim, '_exports_/vae_decoder_onnx', dummy_input, 'vae_decoder',
-                            input_names=['input_1'], output_names=['output_1'])
+    export_quantsim_model(model, '_exports_/vae_decoder_onnx', dummy_input, 'vae_decoder',
+                          input_names=['input_1'], output_names=['output_1'])
+
+
+def export_all_models(te_sim, unet_sim, vae_sim, tokens, embeddings, latents):
+
+    export_text_encoder(te_sim, tokens)
+    export_unet(unet_sim, embeddings)
+    export_vae(vae_sim, latents)
 
 
 def apply_adaround_te(fp_model, adaround_data, config):
-    OUTPUT_PATH = './adaround_model'
+    OUTPUT_PATH = '_exports_/adaround_model'
 
     num_iter = config.adaround_iter_text_encoder
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
 
     def _dummy_fw(model, train_data):
-        return model(train_data[0]) # only adapt adaround for conditional inputs
+        # only adapt adaround for conditional inputs
+        return model(train_data[0])
 
     dummy_input = adaround_data[0][0].to(fp_model.device)
     params = AdaroundParameters(data_loader=adaround_data,
@@ -121,14 +132,16 @@ def apply_adaround_te(fp_model, adaround_data, config):
                                         default_quant_scheme=config.quant_scheme)
 
     # save
-    torch.save(ada_model.state_dict(), os.path.join(OUTPUT_PATH, "state_dict.pt"))
+    torch.save(ada_model.state_dict(), os.path.join(
+        OUTPUT_PATH, "state_dict.pt"))
     quant_sim = QuantizationSimModel(model=ada_model, quant_scheme=config.quant_scheme, dummy_input=dummy_input,
                                      default_output_bw=config.activation_bit_width,
                                      default_param_bw=config.parameter_bit_width,
                                      in_place=config.in_place,  # this will change model if True
                                      config_file=config.config_file)
 
-    quant_sim.set_and_freeze_param_encodings(encoding_path=os.path.join(OUTPUT_PATH, "parameter.encodings"))
+    quant_sim.set_and_freeze_param_encodings(
+        encoding_path=os.path.join(OUTPUT_PATH, "parameter.encodings"))
     return quant_sim
 
 
@@ -154,10 +167,12 @@ def calibrate_te(te_sim, tokens, config):
 def unet_dummy_input(unet_model, embeddings):
     # latent[1,4,64,64], time_emb[1,1280], embed[1,77,768]
     batch_size, h, w = 1, 512, 512
-    dummy_latents = torch.randn((batch_size, unet_model.in_channels, h // 8, w // 8)).to(unet_model.device)
+    dummy_latents = torch.randn(
+        (batch_size, unet_model.in_channels, h // 8, w // 8)).to(unet_model.device)
     time_emb_dim = unet_model.config["block_out_channels"][0] * 4
-    dummy_time_emb = torch.randn((batch_size, time_emb_dim)).to(unet_model.device)
-    dummy_input = embeddings[0].to(unet_model.device)[1:,:,:]
+    dummy_time_emb = torch.randn(
+        (batch_size, time_emb_dim)).to(unet_model.device)
+    dummy_input = embeddings[0].to(unet_model.device)[1:, :, :]
     return dummy_latents, dummy_time_emb, dummy_input
 
 
@@ -167,7 +182,7 @@ def calibrate_unet(model, embeddings, config):
         exceptions.qnn_exceptions(sim, config)
         if config.unet_exception_type.startswith("UNET_attn_"):
             exceptions.qnn_input_exceptions(sim.model,
-                config.unet_exception_type.replace("UNET_attn_", ""), config)
+                                            config.unet_exception_type.replace("UNET_attn_", ""), config)
 
     def _forward_pass_calibration_samples(model, embeddings):
         for embedding in tqdm(embeddings):
@@ -193,7 +208,7 @@ def calibrate_vae(model, latents, config):
         exceptions.qnn_exceptions(sim, config)
         if config.vae_exception_type.startswith("VAE_attn_"):
             exceptions.qnn_input_exceptions(sim.model,
-                config.vae_exception_type.replace("VAE_attn_", ""), config)
+                                            config.vae_exception_type.replace("VAE_attn_", ""), config)
 
     def _forward_pass_calibration_samples(model, latents):
         for latent in tqdm(latents):
--- notebook_main.py
+++ notebook_main.py
@@ -23,26 +23,23 @@
 #  @@-COPYRIGHT-END-@@
 # ==============================================================================
 
+from aimet_quantsim import export_all_models, apply_adaround_te, export_vae, export_unet, export_text_encoder
+from aimet_quantsim import calibrate_vae, calibrate_unet, calibrate_te
+from stable_diff_pipeline import run_the_pipeline, save_image, replace_mha_with_sha_blocks
+from stable_diff_pipeline import run_tokenizer, run_text_encoder, run_diffusion_steps, run_vae_decoder
+from redefined_modules.diffusers.models.vae import AutoencoderKLDecoder
+from redefined_modules.diffusers.models.unet_2d_condition import UNet2DConditionModel
+from redefined_modules.transformers.models.clip.modeling_clip import CLIPTextModel
+from transformers import CLIPTokenizer
+from tqdm import tqdm
+import logging
+import torch
+from argparse import Namespace
+import json
+import os
 import sys
 sys.path.insert(0, '.')
 sys.setrecursionlimit(10000)
-
-import json
-from argparse import Namespace
-
-import torch
-
-from transformers import CLIPTokenizer
-
-from redefined_modules.transformers.models.clip.modeling_clip import CLIPTextModel
-from redefined_modules.diffusers.models.unet_2d_condition import UNet2DConditionModel
-from redefined_modules.diffusers.models.vae import AutoencoderKLDecoder
-
-from stable_diff_pipeline import run_tokenizer, run_text_encoder, run_diffusion_steps, run_vae_decoder
-from stable_diff_pipeline import run_the_pipeline, save_image, replace_mha_with_sha_blocks
-
-from aimet_quantsim import calibrate_vae, calibrate_unet, calibrate_te
-from aimet_quantsim import export_all_models, apply_adaround_te
 
 
 def get_sqnr(org_out, quant_out, in_db=True, eps=1e-10):
@@ -54,78 +51,193 @@ def get_sqnr(org_out, quant_out, in_db=True, eps=1e-10):
     return sqnr_db if in_db else sqnr
 
 
-def load_pretrained_models(cache_dir=None):
-
+def load_text_encoder(cache_dir=None):
     device = 'cuda'
     dtype = torch.float
-
     print("Loading pre-trained TextEncoder model")
     text_encoder = CLIPTextModel.from_pretrained('openai/clip-vit-large-patch14',
                                                  torch_dtype=dtype, cache_dir=cache_dir).to(device)
     text_encoder.config.return_dict = False
+    return text_encoder
 
+
+def load_unet(cache_dir=None):
+    device = 'cuda'
+    dtype = torch.float
     print("Loading pre-trained UNET model")
     unet = UNet2DConditionModel.from_pretrained('runwayml/stable-diffusion-v1-5',
                                                 subfolder="unet", revision='main', torch_dtype=dtype,
                                                 cache_dir=cache_dir).to(device)
     unet.config.return_dict = False
+    return unet
 
+
+def load_vae(cache_dir=None):
+    device = 'cuda'
+    dtype = torch.float
     print("Loading pre-trained VAE model")
     vae = AutoencoderKLDecoder.from_pretrained('runwayml/stable-diffusion-v1-5',
                                                revision='main', subfolder="vae", torch_dtype=dtype,
                                                cache_dir=cache_dir).to(device)
     vae.config.return_dict = False
+    return vae
+
+
+def load_pretrained_models(cache_dir=None):
+
+    text_encoder = load_text_encoder(cache_dir)
+    unet = load_unet(cache_dir)
+    vae = load_vae(cache_dir)
 
     return unet, text_encoder, vae
 
 
-with open('config.json', 'rt') as f:
-    config = Namespace(**json.load(f))
-
-cache_dir = "./_data_/cache/huggingface/diffusers"
-unet, text_encoder, vae = load_pretrained_models(cache_dir=cache_dir)
-
-replace_mha_with_sha_blocks(unet)
-
-tokenizer = CLIPTokenizer.from_pretrained('openai/clip-vit-large-patch14', cache_dir=cache_dir)
-
-prompt = "San Diego downtown and people on the beach"
-image = run_the_pipeline(prompt, unet, text_encoder, vae, tokenizer, test_name='fp32')
-save_image(image.squeeze(0), 'generated.png')
-
-with open(config.calibration_prompts, "rt") as f:
-    print(f'Loading prompts from {config.calibration_prompts}')
-    prompts = f.readlines()
-
-tokens = [run_tokenizer(tokenizer, prompt) for prompt in prompts]
-
-text_encoder_sim = apply_adaround_te(text_encoder, tokens, config)
-
-text_encoder_sim = calibrate_te(text_encoder_sim, tokens, config)
-embeddings = [torch.cat([run_text_encoder(text_encoder_sim.model, uncond),
-               run_text_encoder(text_encoder_sim.model, cond)]) for cond, uncond in tokens]
+def free_cuda_resources(resources):
+    for r in resources:
+        del r
+    torch.cuda.empty_cache()
+    torch.cuda.memory_summary(device=None, abbreviated=False)
 
 
-#############################################################################################
-# ADAROUND PERFORMANCE EVALUATION (can be removed later)
-# test with in_place: false, apply_adaround_text_encoder=true or false
-# with adaround: 17.59, w.o. adaround: 15.119
-org_emb = [torch.cat([run_text_encoder(text_encoder, uncond),
-            run_text_encoder(text_encoder, cond)]) for cond, uncond in tokens]
-sqnr = get_sqnr(torch.stack(org_emb), torch.stack(embeddings))
-print(f"Adaround applied: {config.apply_adaround_text_encoder} | text encoder SQNR: {sqnr}")
-#############################################################################################
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
 
-unet_sim = calibrate_unet(unet, embeddings, config)
-latents = [run_diffusion_steps(unet_sim.model, i) for i in embeddings]
+    script_config = {
+        'runFloatPipeline': False,
+        'textEncoder': {
+            'quantize': False,
+            'embeddingsOutputFolder': '_exports_/embeddings',
+        },
+        'unet': {
+            'quantize': False,
+            'embeddingsInputFolder': '_exports_/embeddings',
+            'latentsOutputFolder': '_exports_/latents',
+        },
+        'vae': {
+            'quantize': True,
+            'latentsInputFolder': '_exports_/latents',
+        },
+    }
 
-vae_sim = calibrate_vae(vae, latents, config)
-images = [run_vae_decoder(vae_sim.model, i) for i in latents]
-for i, image in enumerate(images):
-    save_image(image.squeeze(0), f'generated_after_quant_{i}.png')
+    with open('config.json', 'rt') as f:
+        model_config = Namespace(**json.load(f))
 
+    cache_dir = "./_data_/cache/huggingface/diffusers"
 
-image = run_the_pipeline(prompt, unet_sim.model, text_encoder_sim.model, vae_sim.model, tokenizer, test_name='int8')
-save_image(image.squeeze(0), 'generated_after_quant.png')
+    if script_config['runFloatPipeline']:
+        logging.info("Running the float pipeline")
+        unet, text_encoder, vae = load_pretrained_models(cache_dir=cache_dir)
 
-export_all_models(text_encoder_sim, unet_sim, vae_sim, tokens, embeddings, latents)
+        replace_mha_with_sha_blocks(unet)
+
+        tokenizer = CLIPTokenizer.from_pretrained(
+            'openai/clip-vit-large-patch14', cache_dir=cache_dir)
+
+        prompt = "San Diego downtown and people on the beach"
+        image = run_the_pipeline(prompt, unet, text_encoder,
+                                 vae, tokenizer, test_name='fp32')
+        save_image(image.squeeze(0), 'generated.png')
+        free_cuda_resources([unet, text_encoder, vae])
+
+    with open(model_config.calibration_prompts, "rt") as f:
+        logging.info(
+            f"Loading prompts from {model_config.calibration_prompts}")
+        prompts = f.readlines()
+
+    if script_config['textEncoder']['quantize']:
+        text_encoder = load_text_encoder(cache_dir)
+        tokenizer = CLIPTokenizer.from_pretrained(
+            'openai/clip-vit-large-patch14', cache_dir=cache_dir)
+
+        tokens = [run_tokenizer(tokenizer, prompt) for prompt in prompts]
+
+        logging.info("Applying adaround on text encoder")
+        text_encoder_sim = apply_adaround_te(
+            text_encoder, tokens, model_config)
+
+        logging.info("Calibrating text encoder")
+        text_encoder_sim = calibrate_te(text_encoder_sim, tokens, model_config)
+
+        logging.info("Exporting text encoder")
+        export_text_encoder(text_encoder_sim, tokens)
+        embeddings = [torch.cat([run_text_encoder(text_encoder_sim.model, uncond),
+                                 run_text_encoder(text_encoder_sim.model, cond)]) for cond, uncond in tokens]
+
+        #############################################################################################
+        # ADAROUND PERFORMANCE EVALUATION (can be removed later)
+        # test with in_place: false, apply_adaround_text_encoder=true or false
+        # with adaround: 17.59, w.o. adaround: 15.119
+        org_emb = [torch.cat([run_text_encoder(text_encoder, uncond),
+                              run_text_encoder(text_encoder, cond)]) for cond, uncond in tokens]
+        sqnr = get_sqnr(torch.stack(org_emb), torch.stack(embeddings))
+        logging.info(
+            f"Adaround applied: {model_config.apply_adaround_text_encoder} | text encoder SQNR: {sqnr}")
+        #############################################################################################
+
+        # save the embeddings
+        logging.info('Saving embeddings')
+        for i, emb in enumerate(embeddings):
+            torch.save(emb, os.path.join(
+                script_config['textEncoder']['embeddingsOutputFolder'], f'embedding_{i}.pt'))
+
+        free_cuda_resources(
+            [text_encoder, text_encoder_sim, embeddings, org_emb])
+
+    if script_config['unet']['quantize']:
+
+        unet = load_unet(cache_dir)
+        replace_mha_with_sha_blocks(unet)
+
+        # load embeddings
+        embedding_files = os.listdir(
+            script_config['textEncoder']['embeddingsOutputFolder'])
+        embeddings = [torch.load(os.path.join(
+            script_config['textEncoder']['embeddingsOutputFolder'], f)) for f in embedding_files]
+
+        logging.info("Calibrating unet")
+        unet_sim = calibrate_unet(unet, embeddings, model_config)
+
+        # save latents
+        logging.info('Creating latents')
+        latents = []
+        for emb in tqdm(embeddings):
+            latents.append(run_diffusion_steps(unet_sim.model, emb))
+
+        logging.info('Saving latents')
+        for i, latent in enumerate(latents):
+            logging.info(f"Saving latent {i}")
+            torch.save(latent, os.path.join(
+                script_config['unet']['latentsOutputFolder'], f'latent_{i}.pt'))
+        export_unet(unet_sim, embeddings)
+
+        free_cuda_resources([unet, unet_sim, latents, embeddings])
+
+    if script_config['vae']['quantize']:
+        vae = load_vae(cache_dir)
+
+        # load latents
+        latent_files = os.listdir(
+            script_config['unet']['latentsOutputFolder'])
+        latents = [torch.load(os.path.join(
+            script_config['unet']['latentsOutputFolder'], f)) for f in latent_files]
+
+        logging.info("Calibrating vae")
+        vae_sim = calibrate_vae(vae, latents, model_config)
+        export_vae(vae_sim, latents)
+
+        logging.info('Executing vae decoder with images')
+        images = []
+        for latent in tqdm(latents):
+            images.append(run_vae_decoder(vae_sim.model, latent))
+
+        for i, image in enumerate(images):
+            save_image(image.squeeze(0), f'generated_after_quant_{i}.png')
+
+        # logging.info('Running the quantized pipeline')
+        # image = run_the_pipeline(prompt, unet_sim.model, text_encoder_sim.model,
+        #                         vae_sim.model, tokenizer, test_name='int8')
+        # save_image(image.squeeze(0), 'generated_after_quant.png')
+
+        logging.info('Exporting quantized vae decoder')
+        export_vae(vae_sim, latents)
+        free_cuda_resources([vae, vae_sim, latents])
